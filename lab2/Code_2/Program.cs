8ec15d024d5c8125853376cea4b8b6efbde5515f
BhavinSheth <bhavinsheth67@gmail.com>
2022-02-09 10:04:22 +0530

    done

--- lab2/Code_2/Program.cs
+++ lab2/Code_2/Program.cs
@@ -10,13 +10,16 @@ namespace Code_2
             int b = 6;
             int c = 4;
 
-            if(a + b > 10) {
+            if (a + b > 10)
+            {
                 Console.WriteLine("This answer is greater than 10. ");
-            } else {
+            }
+            else
+            {
                 Console.WriteLine("This answer is greater than 10. ");
             }
 
-            Pro_2.complexIf(a,b,c);
+            Pro_2.complexIf(a, b, c);
 
             // loops
 
@@ -31,74 +34,92 @@ namespace Code_2
         }
     }
 
-    static class Pro_2 {
+    static class Pro_2
+    {
 
-        public static void complexIf(int a, int b, int c) {
+        public static void complexIf(int a, int b, int c)
+        {
 
             Console.WriteLine("Output of complexIf...");
 
-            if ((a + b + c > 10) && (a == b)) {
+            if ((a + b + c > 10) && (a == b))
+            {
                 Console.WriteLine("The answer is greater than 10");
                 Console.WriteLine("And the first number is equal to the second");
             }
-            else {
+            else
+            {
                 Console.WriteLine("The answer is not greater than 10");
                 Console.WriteLine("Or the first number is not equal to the second");
             }
 
-            if ((a + b + c > 10) || (a == b)) {
+            if ((a + b + c > 10) || (a == b))
+            {
                 Console.WriteLine("The answer is greater than 10");
                 Console.WriteLine("And the first number is equal to the second");
             }
-            else {
+            else
+            {
                 Console.WriteLine("The answer is not greater than 10");
                 Console.WriteLine("Or the first number is not equal to the second");
             }
         }
 
-        public static void whileLoop(int counter) {
+        public static void whileLoop(int counter)
+        {
 
             Console.WriteLine("Output of whileLoop...");
 
-            while(counter < 5) {
+            while (counter < 5)
+            {
                 Console.WriteLine($"Hello World! USING While Loop The counter is {counter}");
                 counter++;
             }
         }
-        public static void doWhileLoop(int counter) {
+        public static void doWhileLoop(int counter)
+        {
 
             Console.WriteLine("Output of dowWhileLoop...");
 
-            do {
+            do
+            {
                 Console.WriteLine($"Hello World! USING Do While Loop The counter is {counter}");
                 counter++;
-            } while(counter < 5);
+            } while (counter < 5);
         }
-        public static void forLoop() {
+        public static void forLoop()
+        {
 
             Console.WriteLine("Output of forLoop...");
 
-            for(int counter = 0; counter < 10; counter++) {
+            for (int counter = 0; counter < 10; counter++)
+            {
                 Console.WriteLine($"Hello World! USING For Loop The counter is {counter}");
             }
         }
-        public static void nestLoops() {
+        public static void nestLoops()
+        {
 
             Console.WriteLine("Output of nestLoops...");
 
-            for (int row = 1; row < 11; row++) {
-                for (char column = 'a'; column < 'k'; column++) {
+            for (int row = 1; row < 11; row++)
+            {
+                for (char column = 'a'; column < 'k'; column++)
+                {
                     Console.WriteLine($"The cell is ({row}, {column})");
                 }
             }
         }
-        public static void challenge1() {
+        public static void challenge1()
+        {
 
             Console.WriteLine("Output of challenge1...");
 
             int sum = 0;
-            for (int number = 1; number <= 20; number++) {
-                if (number % 3 == 0) {
+            for (int number = 1; number <= 20; number++)
+            {
+                if (number % 3 == 0)
+                {
                     sum = sum + number;
                 }
             }
